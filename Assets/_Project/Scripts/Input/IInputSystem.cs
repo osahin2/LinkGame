af3b02e6971aabbd6cc12ba87fd75969eb6879fd
Oğuz Şahin <sahnnoguz@gmail.com>
2024-08-19 14:58:54 +0300

    add input system

---
+++ Assets/_Project/Scripts/Input/IInputSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Inputs
+{
+    public interface IInputSystem
+    {
+        public event EventHandler<InputEventArgs> PointerDown;
+        public event EventHandler<InputEventArgs> PointerDrag;
+        public event EventHandler<InputEventArgs> PointerUp;
+        void SetInputActive(bool active);
+        void SetActive(bool active);
+    }
+}
