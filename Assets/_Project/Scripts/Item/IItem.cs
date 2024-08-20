3a7eeed9fc5a0a56200a624bdecb8c6af9275490
Oğuz Şahin <sahnnoguz@gmail.com>
2024-08-20 12:10:13 +0300

    add basic Item implementation

---
+++ Assets/_Project/Scripts/Item/IItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+namespace Item
+{
+    public interface IItem
+    {
+        Transform Transform { get; }
+        void SetSprite(Sprite icon);
+        void Show();
+        void Hide();
+        void SetPosition(Vector3 position);
+    }
+}
---
+++ Assets/_Project/Scripts/Item/LinkItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+namespace Item
+{
+    public class LinkItem : MonoBehaviour, IItem
+    {
+        [SerializeField] private SpriteRenderer _renderer;
+        public Transform Transform => transform;
+
+        public void SetSprite(Sprite icon)
+        {
+            _renderer.sprite = icon;
+        }
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+    }
+}
