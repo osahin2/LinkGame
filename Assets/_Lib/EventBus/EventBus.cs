1a979b361c19c3c30210ef64c8afe2474b7c1e1b
Oğuz Şahin <sahnnoguz@gmail.com>
2024-08-25 04:37:13 +0300

    add EventBus system and events classes

---
+++ Assets/_Lib/EventBus/EventBus.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace EventBusSystem
+{
+    public class EventBus<TEventArgs>
+        where TEventArgs : EventArgs
+    {
+        private static readonly Dictionary<string, Action<TEventArgs>> _eventMap = new();
+
+        public static void RegisterEvent(string eventType, Action<TEventArgs> eventHandler)
+        {
+            if (!_eventMap.ContainsKey(eventType))
+            {
+                _eventMap[eventType] = eventHandler;
+            }
+            else
+            {
+                _eventMap[eventType] += eventHandler;
+            }
+        }
+
+        public static void DeRegisterEvent(string eventType, Action<TEventArgs> eventHandler)
+        {
+            if (_eventMap.ContainsKey(eventType))
+            {
+                _eventMap[eventType] -= eventHandler;
+            }
+        }
+
+        public static void TriggerEvent(string eventType, TEventArgs eventArgs)
+        {
+            if (_eventMap.ContainsKey(eventType))
+            {
+                _eventMap[eventType]?.Invoke(eventArgs);
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Events.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Event
+{
+    public class Events
+    {
+        public const string RESTART_LEVEL = "RestartLevel";
+        public const string NEXT_LEVEL = "NextLevel";
+        public const string LINK_COMPLETED = "LinkCompleted";
+        public const string LEVEL_FAILED = "LevelFailed";
+
+        public class RestartLevel : EventArgs { }
+        public class NextLevel : EventArgs { }
+        public class LinkCompleted : EventArgs
+        {
+            public int NewScore { get; }
+            public int OldScore { get; }
+            public int MoveCount { get; }
+            public LinkCompleted(int newScore, int oldScore, int moveCount)
+            {
+
+                NewScore = newScore;
+                OldScore = oldScore;
+                MoveCount = moveCount;
+            }
+        }
+    }
+}
